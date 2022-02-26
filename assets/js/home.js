5caf6389cb6d8a19b60336f1217d15772b98dabc
Manon Dufour <manon.dufour95@gmail.com>
2022-02-26 17:35:30 +0100

    Travail mobile premier article

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,16 +3,74 @@ let topi = 0;
 
 typewriter(0);
 
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+window.addEventListener("wheel", function(e) {
+    //disableScroll();
+})
+
 window.addEventListener("wheel", function(e) {
     if(e.deltaY > 0 && e.pageY > 0 && e.pageY < 1300) {
         window.scrollTo({
-            top: 1000,
+            top:this.window.pageYOffset + this.window.innerHeight,
             behavior: 'smooth'
           });
     let H2Array = document.querySelector('h2').textContent.split('');
     this.setTimeout(() => {
         writeH2(H2Array, 0, 1);
     }, 500)
+    } else {
+        if(e.deltaY > 0) {
+            this.window.scrollTo({
+                top:this.window.pageYOffset + this.window.innerHeight,
+                behavior:"smooth",
+            })
+        } else {
+            this.window.scrollTo({
+                top:this.window.pageYOffset - this.window.innerHeight,
+                behavior:"smooth"
+            })            
+        }
     }
 })
 
@@ -31,7 +89,33 @@ window.addEventListener("wheel", function(e) {
             duration: 700
         });
         this.document.querySelector('h2').style.filter = `blur(8px)`;
-    } else if(e.deltaY < 0 && e.pageY <= 2050 && this.document.querySelector('h2').style.filter !== 'blur(0px)') {
+    } else if(e.deltaY < 0 && e.pageY <= 2150 && this.document.querySelector('h2').style.filter !== 'blur(0px)') {
+        this.document.querySelector('h2').animate([
+            {filter:'blur(8px)'},
+            {filter:'blur(0px)'}
+        ], {
+            duration: 700
+        });
+        this.document.querySelector('h2').style.filter = `blur(0px)`;
+    }
+}, false);
+
+window.addEventListener("DOMMouseScroll", function(e) {
+    if(e.deltaY > 0 && e.pageY >= 700) {
+        let H2Array = document.querySelector('h2').textContent.split('');
+        writeH2(H2Array, 0, 1);
+        console.log(e.pageY);
+        console.log(e.pageX);
+    };
+    if(e.deltaY > 0 && e.pageY >= 1300 && this.document.querySelector('h2').style.filter !== 'blur(8px)') {
+        this.document.querySelector('h2').animate([
+            {filter:'blur(0px)'},
+            {filter:'blur(8px)'},
+        ], {
+            duration: 700
+        });
+        this.document.querySelector('h2').style.filter = `blur(8px)`;
+    } else if(e.deltaY < 0 && e.pageY <= 2150 && this.document.querySelector('h2').style.filter !== 'blur(0px)') {
         this.document.querySelector('h2').animate([
             {filter:'blur(8px)'},
             {filter:'blur(0px)'}
@@ -43,8 +127,8 @@ window.addEventListener("wheel", function(e) {
 });
 
 window.addEventListener('wheel', function(e) {
-    if(e.deltaY > 0 && e.pageY >= 900 && this.document.querySelector('.blackContainer').style.visibility !== "visible" && !this.document.querySelector('#blackContainer__Appearance').style.transform) {
-        let animation = this.document.querySelector('#blackContainer__Appearance').animate([
+    if(e.deltaY > 0 && e.pageY >= 900 && this.document.querySelector('#visualIdentity').style.visibility !== "visible" && !this.document.querySelector('#visualIdentityBlock__AppearanceDiv').style.transform) {
+        let animation = this.document.querySelector('#visualIdentityBlock__AppearanceDiv').animate([
             {transform:'scale(0,0)', opacity:1},
             {transform: 'scale(0.05, 1)', opacity:1},
            {transform: 'scale(1,1)', opacity:1},
@@ -52,17 +136,17 @@ window.addEventListener('wheel', function(e) {
            duration: 500
        });
        animation;
-       this.document.querySelector('#blackContainer__Appearance').style.transform = "scale(1,1)";
+       this.document.querySelector('#visualIdentityBlock__AppearanceDiv').style.transform = "scale(1,1)";
        animation.onfinish = event => {
         this.setTimeout(() => {
-            this.document.querySelector('#blackContainer__Appearance').animate([
+            this.document.querySelector('#visualIdentityBlock__AppearanceDiv').animate([
                 {opacity:1},
                 {opacity:0}
             ], {
                 duration: 200,
             });
-            this.document.querySelector('.blackContainer').style.visibility = "visible";
-            this.document.querySelector('#blackContainer__Appearance').style.opacity = 0;
+            this.document.querySelector('#visualIdentity').style.visibility = "visible";
+            this.document.querySelector('#visualIdentityBlock__AppearanceDiv').style.opacity = 0;
         }, 300);
        }
        
