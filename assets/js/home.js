929ebbd2278ce775b06b5c2a52407af78716da61
Manon Dufour <manon.dufour95@gmail.com>
2022-02-26 17:57:50 +0100

    Essais mobile partie identité visuelle

--- assets/js/home.js
+++ assets/js/home.js
@@ -98,33 +98,8 @@ window.addEventListener("wheel", function(e) {
         });
         this.document.querySelector('h2').style.filter = `blur(0px)`;
     }
-}, false);
+});
 
-window.addEventListener("touchmove", function(e) {
-    if(e.deltaY > 0 && e.pageY >= 700) {
-        let H2Array = document.querySelector('h2').textContent.split('');
-        writeH2(H2Array, 0, 1);
-        console.log(e.pageY);
-        console.log(e.pageX);
-    };
-    if(e.deltaY > 0 && e.pageY >= 1300 && this.document.querySelector('h2').style.filter !== 'blur(8px)') {
-        this.document.querySelector('h2').animate([
-            {filter:'blur(0px)'},
-            {filter:'blur(8px)'},
-        ], {
-            duration: 700
-        });
-        this.document.querySelector('h2').style.filter = `blur(8px)`;
-    } else if(e.deltaY < 0 && e.pageY <= 2150 && this.document.querySelector('h2').style.filter !== 'blur(0px)') {
-        this.document.querySelector('h2').animate([
-            {filter:'blur(8px)'},
-            {filter:'blur(0px)'}
-        ], {
-            duration: 700
-        });
-        this.document.querySelector('h2').style.filter = `blur(0px)`;
-    }
-}, false);
 
 window.addEventListener('wheel', function(e) {
     if(e.deltaY > 0 && e.pageY >= 900 && this.document.querySelector('#visualIdentity').style.visibility !== "visible" && !this.document.querySelector('#visualIdentityBlock__AppearanceDiv').style.transform) {
